716824d2de7881b1ff8b329488d1566127cb1dd4
facrin222 <mandu0611@naver.com>
2026-09-16 22:59:17 +0900

    Update script.js
    Signed-off-by: facrin222 <mandu0611@naver.com>

--- main/script.js
+++ main/script.js
@@ -1,27 +1,30 @@
 let clickCount = 0;
-
+let clickPerSec = 0;
 
 const button = document.getElementById("clickButton");
 const clickCountText = document.getElementById("clickCount");
+const clickPerSecText = document.getElementById("clickPerSec");
 
+let clickTimes = [];
 
-//TC, CPS count
 button.addEventListener("click", () => {
+    // Total Click 증가
     clickCount++;
-
-    clickCountInSecond++;
-
     clickCountText.textContent = clickCount;
+
+    // 클릭한 시간 기록
+    clickTimes.push(Date.now());
 });
 
-//count clicks per second in recent 1s
-let clickPerSec = 0;
-const clickPerSecText = document.getElementById("clickPerSec");
-let clickCountInSecond = 0;
-
 setInterval(() => {
-    clickPerSec = clickCountInSecond;
-    clickPerSecText.textContent = clickPerSec;
+    const now = Date.now();
 
-    clickCountInSecond = 0;
-}, 1000);
+    // 최근 1초 이내의 클릭만 남김
+    clickTimes = clickTimes.filter(time => now - time < 1000);
+
+    // 최근 1초간 클릭 횟수
+    clickPerSec = clickTimes.length;
+
+    // 화면에 표시
+    clickPerSecText.textContent = clickPerSec;
+}, 50);
