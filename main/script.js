c99e4c91797f915784927880350ede7f7f7ab6dd
facrin222 <mandu0611@naver.com>
2026-09-18 14:09:56 +0000

    Pending changes exported from your codespace

--- main/script.js
+++ main/script.js
@@ -28,3 +28,51 @@ setInterval(() => {
     // 화면에 표시
     clickPerSecText.textContent = clickPerSec;
 }, 50);
+
+// ===== Side Panel Module =====
+
+const sidePanel = document.getElementById("sidePanel");
+const sideTabs = document.querySelectorAll(".side-tab");
+const sideTabContents = document.querySelectorAll(".side-tab-content");
+
+
+// TC가 10을 넘었을 때 Side Panel 해금
+button.addEventListener("click", () => {
+    if (clickCount >= 10) {
+        sidePanel.style.display = "block";
+    }
+});
+
+
+// Tab 전환
+sideTabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+
+        // 모든 탭 비활성화
+        sideTabs.forEach(tab => {
+            tab.classList.remove("active");
+
+            const current = tab.querySelector(".tab-current");
+            current.textContent = "";
+        });
+
+        // 클릭한 탭 활성화
+        tab.classList.add("active");
+
+        const current = tab.querySelector(".tab-current");
+        current.textContent = "C";
+
+
+        // 모든 내용 숨김
+        sideTabContents.forEach(content => {
+            content.classList.remove("active");
+        });
+
+
+        // 선택한 내용 표시
+        const targetId = tab.dataset.tab;
+        const targetContent = document.getElementById(targetId);
+
+        targetContent.classList.add("active");
+    });
+});
